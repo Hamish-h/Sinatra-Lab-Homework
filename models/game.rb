4269e74851a2551fe5ea0728601c07f51674929e
davina <davina.kuhnel@gmail.com>
2018-05-01 15:18:19 +0100

    initial files created

---
+++ models/game.rb
@@ -0,0 +1,16 @@
+
+
+# if rock && scissors
+#   "rock wins"
+# if scissors && rock
+#   "rock wins"
+
+# if scissors && paper
+# "scissors wins"
+# if paper && scissors
+# "scissors wins"
+
+# if paper && rock
+#   "paper wins"
+# # if rock && paper
+#   "paper wins"
